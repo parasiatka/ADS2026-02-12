930d4e8dd7f476212c56f82393b8d2a8d2eb810f
Mikita <mikita.nekhv@gmail.com>
2026-02-19 14:49:52 +0300

    done: C

--- src/by/it/group510901/nekhviadovich/lesson01/FiboC.java
+++ src/by/it/group510901/nekhviadovich/lesson01/FiboC.java
@@ -6,6 +6,9 @@ package by.it.group510901.nekhviadovich.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -22,10 +25,35 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
-        //Интуитивно найти решение не всегда просто и
-        //возможно потребуется дополнительный поиск информации
-        return -1L;
+        if(n < 1){ return 0L; }
+        if(n == 1) { return 1L; }
+        long reducedN = n%getPisano(m);//n in m range
+
+        ArrayList<Long> fiboNumbers = new ArrayList<>();
+        fiboNumbers.add(0L);
+        fiboNumbers.add(1L);
+        for (int i = 2; i <= reducedN; i++){
+            fiboNumbers.add(fiboNumbers.get(i-2) + fiboNumbers.get(i-1));
+        }
+        long nFibo = fiboNumbers.get((int)reducedN);
+        return nFibo % m;
     }
+
+    int getPisano(int m){
+
+        int prev = 0;
+        int curr = 1;
+
+        for(int i = 0; i < m * 10; i++){ // m*6 is actual maximum
+            if(prev == 0 && curr == 1 && i > 0){ return i; }
+            int nextMod = (prev + curr) % m;
+            prev = curr;
+            curr = nextMod;
+        }
+        return 0;
+    }
+
+
 
 
 }
